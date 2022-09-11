a25c73fc253815900f2897dd4bd06a1bf5677407
DannOut <danielxdn@gmail.com>
2022-09-11 20:44:24 -0300

    addind simple sass

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,13 @@
 import React from 'react';
+import categories from './Data/Categories';
+import './categories.styles.scss';
 
 function App() {
-  const categories = [
-    {
-      id: 1,
-      title: 'Hats',
-    },
-    {
-      id: 2,
-      title: 'Jackets',
-    },
-    {
-      id: 3,
-      title: 'Sneakers',
-    },
-    {
-      id: 4,
-      title: 'Mens',
-    },
-    {
-      id: 5,
-      title: 'Womens',
-    },
-  ];
-
   return (
     <div className="categories-container">
-      {categories.map(({ title }) => (
-        <div className="category-container">
+      {categories.map(({ title, id }) => (
+        <div key={id} className="category-container">
+          <div className="background-image" />
           <div className="category-body-container">
             <h2>{title}</h2>
             <p> Shop Now </p>
---
+++ src/Data/Categories.js
@@ -0,0 +1,24 @@
+const categories = [
+  {
+    id: 1,
+    title: 'Hats',
+  },
+  {
+    id: 2,
+    title: 'Jackets',
+  },
+  {
+    id: 3,
+    title: 'Sneakers',
+  },
+  {
+    id: 4,
+    title: 'Mens',
+  },
+  {
+    id: 5,
+    title: 'Womens',
+  },
+];
+
+export default categories;
