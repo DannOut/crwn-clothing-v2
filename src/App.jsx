e9febf4172c19b8d32a508770298387d560a47d5
DannOut <danielxdn@gmail.com>
2022-09-11 19:53:18 -0300

    starting paralel project

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+function App() {
+  const categories = [
+    {
+      id: 1,
+      title: 'Hats',
+    },
+    {
+      id: 2,
+      title: 'Jackets',
+    },
+    {
+      id: 3,
+      title: 'Sneakers',
+    },
+    {
+      id: 4,
+      title: 'Mens',
+    },
+    {
+      id: 5,
+      title: 'Womens',
+    },
+  ];
+
+  return (
+    <div className="categories-container">
+      {categories.map(({ title }) => (
+        <div className="category-container">
+          <div className="category-body-container">
+            <h2>{title}</h2>
+            <p> Shop Now </p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
